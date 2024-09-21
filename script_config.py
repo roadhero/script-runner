9af1e8a44900a16fc2f81aec6ff9d3c465551cbe
Denys Vorobyov <roadhero@users.noreply.github.com>
2024-09-21 01:12:39 +0100

    Add files via upload

---
+++ script_config.py
@@ -0,0 +1,16 @@
+SCRIPTS = [
+    {
+        "name": "My Python Script",
+        "path": "~/projects/myscript.py",
+        "working_dir": "~/projects",
+        "is_python": True,
+        "args": "-v --output result.txt"
+    },
+    {
+        "name": "Custom App",
+        "path": "~/apps/myapp",
+        "working_dir": "~/apps",
+        "is_python": False,
+        "args": "-t 0 -d 1 -gpu"
+    },
+]
